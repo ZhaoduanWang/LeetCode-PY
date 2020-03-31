1953b46883e069d8d5c190d2a8fd25d094ffc876
Danny Wong <dannywang0619@gmail.com>
2020-03-31 15:39:20 -0600

    LctM0015

--- LtcM0015ThreeSum.py
+++ LtcM0015ThreeSum.py
@@ -4,28 +4,26 @@ class Solution(object):
         sortNums = nums.copy()
         sortNums.sort()
 
-        for i in range(0, len(nums)-2):
-            if ( (i==0) or (i>0 and nums[i]!=nums[i-1]) ):
+        for i in range(0, len(sortNums)-2):
+            if ( (i==0) or (i>0 and sortNums[i]!=sortNums[i-1]) ):
                 low = i+1
-                high = len(nums)-1
-                sum = 0-nums[i]
+                high = len(sortNums)-1
+                sum = 0-sortNums[i]
 
                 while(low<high):
-                    if ( (nums[low]+nums[high])==sum):
-                        rt.add(tuple([nums[i], nums[low], nums[high]]))
-                        while ( (low<high) and (nums[low]==nums[low+1]) ):
+                    if ( (sortNums[low]+sortNums[high])==sum):
+                        rt.add(tuple([sortNums[i], sortNums[low], sortNums[high]]))
+                        while ( (low<high) and (sortNums[low]==sortNums[low+1]) ):
                             low += 1
-                        while ( (low<high) and (nums[high]==nums[high-1]) ):
+                        while ( (low<high) and (sortNums[high]==sortNums[high-1]) ):
                             high -=  1
                         low += 1
                         high -= 1
-                    elif ( (nums[low]+nums[high])>sum ):
+                    elif ( (sortNums[low]+sortNums[high])>sum ):
                         high -= 1
                     else:
                         low += 1
-
-        print(rt)
         return rt
 
 sol = Solution()
-sol.threeSum([-1, 0, 1, 2, -1, -4])
+print(sol.threeSum([-1, 0, 1, 2, -1, -4]))
