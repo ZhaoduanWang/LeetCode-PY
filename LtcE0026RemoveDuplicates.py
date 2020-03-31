12c65b8a80a31441031d7d0a932e04e3d536eec9
Danny Wong <dannywang0619@gmail.com>
2020-03-30 21:49:38 -0600

    LtcM0015

---
+++ LtcE0026RemoveDuplicates.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        max = nums[len(nums)-1]
+        prev = max
+
+        for i in range(len(nums)):
+            num = nums.pop(0)
+            if (num!=prev):
+                nums.append(num)
+                prev = num
+
+        return len(nums)
+
+sol = Solution()
+print(sol.removeDuplicates([1,1,2]))
+print(sol.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
